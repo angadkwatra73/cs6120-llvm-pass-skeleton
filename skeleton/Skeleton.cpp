596855e3632eed460c6b21d134588b356240c4e8
angadkwatra73 <angadsinghkwatra@gmail.com>
2026-03-28 11:04:08 +0530

    ridiculous passs

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -3,15 +3,42 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/IRBuilder.h"
 
 using namespace llvm;
 
 namespace {
 
+// so we wanna do two things - pattern match 
+// then do the transform 
+// this is similar to lowering pass in MLIR - where you match some pattern, then rewrite
+
 struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
         for (auto &F : M) {
             errs() << "I saw a function called " << F.getName() << "!\n";
+            errs() << "Function body is: \n";
+            errs() << F << "\n";
+            for (auto &B : F) {
+              for (auto& I : B) {
+                if (auto* op = dyn_cast<BinaryOperator>(&I)) {
+                  errs() << *op << "\n";
+                  //used IR BUilder class to build instructoins
+                  IRBuilder<> builder(op);
+                  Value* lhs = op->getOperand(0);
+                  Value* rhs = op->getOperand(1);
+                  Value *mul = builder.CreateMul(lhs, rhs);
+                  //replace all uses of old instruction with the new one
+                  // the old instruction will be dead code then automatically
+                  for (auto& U : op->uses()) {
+                    User* user = U.getUser();
+                    user ->setOperand(U.getOperandNo(), mul);
+
+                  }
+                }
+              }
+            }
         }
         return PreservedAnalyses::all();
     };
---
+++ something.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+  /*
+  int x = 10 ;
+  int y = 6;
+
+  int z = x + y;
+  printf("%d", z);
+  */
+  return argc + 5;
+}
+
+
